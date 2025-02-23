cface8f67e4314bd1cf96656201a6152403127bc
murodjon <murodjonisroiljonov4@gmail.com>
2025-02-24 00:39:33 +0500

    cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,10 +2,24 @@ import React from "react";
 import { useContext } from "react";
 import { CartContext } from "../App";
 
+
 function Cart() {
   const { cart, setCart } = useContext(CartContext)
+  console.log("cart", cart);
+  return (
+    <div>
 
-  return <div className="">Cart</div>;
+      {cart.length > 0 && cart.map(function (item, index) {
+        return <div key={index}>
+          <img src={item?.product.attributes.image} className="w-[300px]" />
+          {
+            console.log("image", item?.product.attributes.image)
+
+          }
+        </div>
+      })}
+    </div>
+  );
 }
 
 export default Cart;
